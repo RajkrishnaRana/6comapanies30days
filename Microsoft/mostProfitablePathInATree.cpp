886abf3432590f5af8ada0a13746fd33bd03593a
Rajkrishna Rana <95145115+RajkrishnaRana@users.noreply.github.com>
2023-01-05 11:10:42 +0530

    Update mostProfitablePathInATree.cpp

--- Microsoft/mostProfitablePathInATree.cpp
+++ Microsoft/mostProfitablePathInATree.cpp
@@ -1,89 +1,76 @@
 class Solution {
 public:
-    vector<int>adj[200001];
-    int vis[200001],dis[200001];
-    int par[200001];
-     int dp[200001];
-    void dfs(int x,int p)
-    {
-      par[x]=p;
-        for(int k:adj[x])
-        {
-            if(k!=p)
-            {
-                dfs(k,x);
-            }
+    vector<int>g[100005];
+    int level_bob[100005];
+    bool vis[100005];
+    
+    int pa[100005];
+    
+    long long max_amount=-1e9;
+    vector<int>Amount;
+public:
+    int mostProfitablePath(vector<vector<int>>& edges, int bob, vector<int>& amount) {
+        Amount = amount;
+        for(auto v: edges){
+            g[v[0]].push_back(v[1]);
+            g[v[1]].push_back(v[0]);
         }
-    }
-    int solve(int x,int d,vector<int>&a,int p)
-    {
-        int ok=0;
-        int ans=-1e9;
-        if(dis[x]!=-1)
-        {
-            if(dis[x]<d)
-            {
-                ok=0;
-            }else if(dis[x]==d)
-            {
-                ok=a[x]/2;
-            }else
-            {
-                ok=a[x];
-            }
-        }else
-        {
-            ok=a[x];
-        }
-
-        int cur=-1e9;
-        int check=0;
-        for(int j:adj[x])
-        {
-            if(j!=p)
-            {
-                check=1;
-                solve(j,d+1,a,x);
-                cur=max(cur,dp[j]);
-            }
-        }
-    if(!check)
-    {
-        cur=0;
-    }
-
-        ok=ok+cur;
-        cout<<ok<<" "<<x<<endl;
-        ans=ok;
-        return dp[x]=ans;
         
+        for(int i=0; i<edges.size()+1; i++) level_bob[i]=1e9;
+        memset(vis, false , sizeof(vis));
+        level_bob[bob]=0;
+        dfs_bob(bob, -1);
+        
+        map<int, bool>mpp;
+        int node=0;
+        while(pa[node]!=-1){
+            mpp[node]=true;
+            node =pa[node];
+        }
+        mpp[bob]=true;
+        
+        for(int i=0; i<edges.size()+1; i++)
+        {
+            if(!mpp[i]){ 
+                level_bob[i]=1e9;
+            }
+        }
+        
+       
+        
+        memset(vis, false, sizeof(vis));
+        int level=0;
+        long long sum_amount=0;
+        dfs_alice(0, level, sum_amount);
+        
+        return (int)max_amount;
     }
     
-    int mostProfitablePath(vector<vector<int>>& e, int bob, vector<int>& a) {
-        for(vector<int>x:e)
-        {
-            int a1=x[0]; int b=x[1];
-            adj[a1].push_back(b);
-            adj[b].push_back(a1);
+    void dfs_bob(int u, int p)
+    {
+        vis[u]=true;
+        pa[u]=p;  /// parent set
+        for(auto v: g[u]){
+            if(!vis[v]){
+                level_bob[v]=level_bob[u]+1; 
+                dfs_bob(v, u);
+            }
         }
-        memset(dis,-1,sizeof(dis));
-        memset(dp,-1,sizeof(dp));
-        dfs(0,-1);
-        vector<int>path;
-        int ok= bob;
-        while(ok!=0)
-        {
-            path.push_back(ok);
-            ok=par[ok];
+    }
+    
+    void dfs_alice(int u, int level, long long sum_amount)
+    {
+        vis[u]=true;
+        if(level<level_bob[u]) sum_amount+=Amount[u]; 
+        else if(level==level_bob[u]) sum_amount+=(Amount[u]/2);
+     
+        if( u!=0 && g[u].size()==1){ 
+            max_amount=max(max_amount, sum_amount);
         }
-      path.push_back(0);
-       dis[bob]=0;
-        for(int i=1;i<path.size();i++)
-        {
-            dis[path[i]]=i;
+        for(auto v: g[u]){
+            if(!vis[v]){
+                dfs_alice(v, level+1, sum_amount);
+            }
         }
-       int ans= solve(0,0,a,-1);
-        
-        return ans;
     }
 };
