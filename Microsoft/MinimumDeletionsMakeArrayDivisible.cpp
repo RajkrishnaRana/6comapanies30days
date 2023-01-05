ea6439e8fdd4d480f3c2cfebc055a99ae6692ebc
Rajkrishna Rana <95145115+RajkrishnaRana@users.noreply.github.com>
2023-01-05 15:28:12 +0530

    Update MinimumDeletionsMakeArrayDivisible.cpp

--- Microsoft/MinimumDeletionsMakeArrayDivisible.cpp
+++ Microsoft/MinimumDeletionsMakeArrayDivisible.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, vector<int>& numsD) {
-        int ans = -1;
-        int n = nums.size();
-        sort(nums.begin() , nums.end());
-        int gcdd = *max_element(numsD.begin() , numsD.end());
-        
-        for(auto i:numsD)
-        {
-            gcdd = __gcd(gcdd , i);
+        sort(nums.begin(), nums.end());
+        sort(numsDivide.begin(), numsDivide.end());
+        int cnt=0;
+        int cmn=numsDivide[0];
+        for(int i=1; i<numsDivide.size(); i++){
+            cmn=__gcd(cmn, numsDivide[i]);
         }
-        
-        for(int i=0;i<n;++i)
-        {
-            if(gcdd%nums[i] == 0)
-            {
-            ans = i;
-                break;
-            } 
+        //cout<<cmn<<endl;
+        if(nums[0]>cmn){return -1;}
+        for(int i=0; i<nums.size(); i++){
+            if(cmn%nums[i]==0){
+                return cnt;
+            }
+            cnt++;
         }
-        return ans;
+        return -1;
+
     }
 };
